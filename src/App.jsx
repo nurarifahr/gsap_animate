3969a831d9ec240aeda0975fed772214ea301077
nurarifahr <2020303554@mhs.udb.ac.id>
2025-09-16 21:00:23 +0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all'
+import Navbar from './components/Navbar';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-5xl text-indigo-500'>Hello GSAP</h1>
-    </div>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
