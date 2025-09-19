1f7cd13d48cbd267340cbfdd0dfd9dc3f65c8eb4
nurarifahr <202030354@mhs.udb.ac.id>
2025-09-19 23:22:23 +0700

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cokctails from './components/Cokctails';
+import About from './components/About';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +14,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Cokctails/>
+      <About/>
     </main>
   )
 }
