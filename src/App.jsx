84071a2e89902c89fb06e349d698f58564831a54
nurarifahr <202030354@mhs.udb.ac.id>
2025-09-20 20:43:45 +0700

    menu section and contact section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Hero from './components/Hero';
 import Cokctails from './components/Cokctails';
 import About from './components/About';
 import Art from './components/Art';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -17,6 +19,8 @@ const App = () => {
       <Cokctails/>
       <About/>
       <Art/>
+      <Menu/>
+      <Contact/>
     </main>
   )
 }
